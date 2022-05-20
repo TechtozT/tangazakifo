7df60398c658d6d98b5d97572635bfd6391a750d
TechtozT <techtoz08@gmail.com>
2022-05-20 05:37:45 +0300

    Set path variable okay

--- src/main/java/com/tangazakifo/tangazakifo/BasicController.java
+++ src/main/java/com/tangazakifo/tangazakifo/BasicController.java
@@ -1,15 +1,22 @@
 package com.tangazakifo.tangazakifo;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/hello")
 public class BasicController {
 
-  @GetMapping("/")
+  @GetMapping
   String hello() {
     return "Hello There";
+  }
+
+  @GetMapping("/{id}")
+  String getById(@PathVariable String id) {
+    System.out.println(id);
+    return "You requested resource with id " + id;
   }
 }
