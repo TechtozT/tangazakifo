fa8ac51b7ccc2a9a31d9d6be585f2be449125c17
TechtozT <techtoz08@gmail.com>
2022-05-20 05:19:04 +0300

    Init project

---
+++ src/main/java/com/tangazakifo/tangazakifo/TangazakifoApplication.java
@@ -0,0 +1,12 @@
+package com.tangazakifo.tangazakifo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TangazakifoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TangazakifoApplication.class, args);
+	}
+}
---
+++ src/test/java/com/tangazakifo/tangazakifo/TangazakifoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tangazakifo.tangazakifo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TangazakifoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
